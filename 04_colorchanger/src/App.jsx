16b8eef8ec7ccf9e02436a36802776c55e7fbdcf
Sohom Das <39939163+Sohomz@users.noreply.github.com>
2024-09-19 05:43:51 +0000

    bgColor change, h-screen, pass bgColor to the <div> where it needed to be changed <div className="flex h-screen w-full" style={{backgroundColor: `${bgColor}`}}>

--- 04_colorchanger/src/App.jsx
+++ 04_colorchanger/src/App.jsx
@@ -3,23 +3,21 @@ import { useState } from 'react';
 import React from 'react';
 
 function App() {
-  const [bgColor, setBgColor] = useState('');
+  const [bgColor, setBgColor] = useState('Black');
 
-  const colorChange = (clr) => {
-    setBgColor(clr);
-    document.body.className = clr;
-  };
+  // onClick always want function, but not the return of a function, thats why we have to pass a function, else callback ()=>
 
   return (
-    <>
-      <div className={`fixed bottom-0 w-full flex justify-center p-4 space-x-6 ${bgColor}`}>
-        <button onClick={() => colorChange('bg-red-500')} className="bg-red-500 text-white px-4 py-2 rounded cursor-pointer">Red</button>
-        <button onClick={() => colorChange('bg-green-500')} className="bg-green-500 text-white px-4 py-2 rounded">Green</button>
-        <button onClick={() => colorChange('bg-blue-500')} className="bg-blue-500 text-white px-4 py-2 rounded">Blue</button>
-        <button onClick={() => colorChange('bg-white')} className="bg-white text-black px-4 py-2 rounded border-black">White</button>
-        <button onClick={() => colorChange('bg-zinc-900')} className="bg-zinc-900 text-white px-4 py-2 rounded">Dark</button>
+       <div className="flex h-screen w-full" style={{backgroundColor: `${bgColor}`}}>
+          {/*Passing bgColor vaiable here so that the color can be changed, we had to use h-screen to take the full height, also we need to pass raw colors, not the bg-green-500 etc, this will not work in js*/}
+        <div className="fixed bottom-0 w-full flex justify-center">
+          <button onClick={() => setBgColor('red')} className="flex bg-red-500 text-white px-4 py-2 rounded cursor-pointer">Red</button>
+          <button onClick={() => setBgColor('green')} className="flex bg-green-500 text-white px-4 py-2 rounded">Green</button>
+          <button onClick={() => setBgColor('blue')} className="flex bg-blue-500 text-white px-4 py-2 rounded">Blue</button>
+          <button onClick={() => setBgColor('white')} className="flex bg-white text-black px-4 py-2 rounded border-black">White</button>
+          <button onClick={() => setBgColor('#313131')} className="flex bg-zinc-800 text-white px-4 py-2 rounded">Dark</button>
+        </div>
       </div>
-    </>
   );
 }
 
