7a7e7115f32332bf6fb1563bde05d13cec9dfeef
Sohomz <sohomd088@gmail.com>
2024-10-27 13:39:44 +0530

    context api react for global variable type

---
+++ 08_recatContext/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='flex text-white justify-center items-center w-screen h-screen text-2xl font-bold'>This is for react</div>
+    </>
+  )
+}
+
+export default App
