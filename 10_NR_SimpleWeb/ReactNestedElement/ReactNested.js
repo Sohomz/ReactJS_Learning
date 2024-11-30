03adac7d25e271c37d77ca16109b28fc1517aa73
Sohomz <sohomd088@gmail.com>
2024-11-30 11:40:10 +0530

    NR_EP1_end

---
+++ 10_NR_SimpleWeb/ReactNestedElement/ReactNested.js
@@ -0,0 +1,19 @@
+//what to render?
+const heading=React.createElement("div",{id:"parent",xyz:"abc"},
+    [
+        React.createElement("div",{id:"idForChildDiv1"},
+            [React.createElement("h1",{id:"idForNestedH1"},"From Nested H1 using React Create Element"),
+            React.createElement("h2",{id:"idForNestedH1"},"From Nested H2 using React Create Element")]
+            ), //here we start child2 because this is an array
+        React.createElement("div",{id:"idForChildDiv2"},
+            [React.createElement("h3",{id:"idForNestedH3"},"From Nested H3 using React Create Element"),
+            React.createElement("h4",{id:"idForNestedH4"},"From Nested H4 using React Create Element")]
+            )
+    ]
+);
+
+//Where to render
+const r=ReactDOM.createRoot(document.getElementById("root"));
+
+//Now render inside div that is to say root id
+r.render(heading);
