a3e332a6b8c17418f223795e1dff21bbf98c27b3
Sohomz <sohomd088@gmail.com>
2024-12-09 15:06:34 +0530

    video 3, 22 min

--- 10_NR_SimpleWeb/ReactNestedElement/ReactNested.js
+++ 10_NR_SimpleWeb/ReactNestedElement/ReactNested.js
@@ -1,19 +1,23 @@
-//what to render?
-const heading=React.createElement("div",{id:"parent",xyz:"abc"},
-    [
-        React.createElement("div",{id:"idForChildDiv1"},
-            [React.createElement("h1",{id:"idForNestedH1"},"From Nested H1 using React Create Element"),
-            React.createElement("h2",{id:"idForNestedH1"},"From Nested H2 using React Create Element")]
-            ), //here we start child2 because this is an array
-        React.createElement("div",{id:"idForChildDiv2"},
-            [React.createElement("h3",{id:"idForNestedH3"},"From Nested H3 using React Create Element"),
-            React.createElement("h4",{id:"idForNestedH4"},"From Nested H4 using React Create Element")]
-            )
-    ]
-);
+import React from "react";
+import ReactDOM from "react-dom";
 
-//Where to render
-const r=ReactDOM.createRoot(document.getElementById("root"));
+// //what to render?
+// const heading=React.createElement("div",{id:"parent",xyz:"abc"},
+//     [
+//         React.createElement("div",{id:"idForChildDiv1"},
+//             [React.createElement("h1",{id:"idForNestedH1"},"From Nested H1 using React Create Element"),
+//             React.createElement("h2",{id:"idForNestedH1"},"From Nested H2 using React Create Element")]
+//             ), //here we start child2 because this is an array
+//         React.createElement("div",{id:"idForChildDiv2"},
+//             [React.createElement("h3",{id:"idForNestedH3"},"From Nested H3 using React Create Element"),
+//             React.createElement("h4",{id:"idForNestedH4"},"From Nested H4 using React Create Element")]
+//             )
+//     ]
+// );
 
-//Now render inside div that is to say root id
-r.render(heading);
+// //Where to render
+// const r=ReactDOM.createRoot(document.getElementById("root"));
+
+// //Now render inside div that is to say root id
+// r.render(heading);
+
