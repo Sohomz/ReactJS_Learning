519772d4ad7f3838a1eb431f0ec4f4e77dcf1e4f
Sohomz <sohomd088@gmail.com>
2025-01-12 12:53:00 +0530

    shimmer added

--- 11_NR_Mock_Web/src/components/Body.js
+++ 11_NR_Mock_Web/src/components/Body.js
@@ -1,42 +1,52 @@
 import Card from "./Card.js";
 import SearchBox from "./SearchBox.js";
-import resturantList from "../utils/mockData.js";
 import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer.js";
+import resturantList from "../utils/mockData.js";
 
 const Body = () => {
   const [listToUpdate, setListToUpdate] = useState(resturantList.restaurants); // this is to update the list of restaurants
+  const [error, setError] = useState(false); // this is to handle the error state
+
   useEffect(() => {
     fetchData();
   }, []);
 
   const fetchData = async () => {
-    const fetchedAPI = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.867114&lng=88.3674381&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-    );
-    const jsonConverted = await fetchedAPI.json();
-    console.log(
-      jsonConverted.data.cards[2].card.card.gridElements.infoWithStyle
-        .restaurants
-    );
+    try {
+      const fetchedAPI = await fetch(
+        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.867114&lng=88.3674381&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      );
+      const jsonConverted = await fetchedAPI.json();
+      const dataOfRestuarant =
+        jsonConverted.data.cards[2].card.card.gridElements.infoWithStyle
+          .restaurants;
+      console.log(dataOfRestuarant);
+    } catch (error) {
+      setError(true);
+    }
   };
 
   return (
     <div>
-      <div className="res-container">
-        {/* <SearchBox /> */}
-        <SearchBox
-          listToUpdate={listToUpdate}
-          setListToUpdate={setListToUpdate}
-        />
-        {/* //I have to pass it because searchBox has the filterBox component
-        although Searchbox dont need it, Prop drilling */}
-        {listToUpdate.map((i) => (
-          <Card passData={i.info} key={i.info.id} />
-        ))}
-      </div>
+      <SearchBox
+        listToUpdate={listToUpdate}
+        setListToUpdate={setListToUpdate}
+      />
+
+      {listToUpdate.length === 0 || error ? (
+        <div>
+          <Shimmer />
+        </div>
+      ) : (
+        <div className="res-container">
+          {listToUpdate.map((i) => (
+            <Card passData={i.info} key={i.info.id} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
-// TODO: Fetch data from API directly and pass it to the Card component
 
 export default Body;
