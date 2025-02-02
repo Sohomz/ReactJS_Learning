02bc2c35edc225afbe5d2aeac445f042fdbc31c9
Sohomz <sohomd088@gmail.com>
2025-02-02 11:36:11 +0530

    56 mins

--- 11_NR_Mock_Web/src/components/UserClass.js
+++ 11_NR_Mock_Web/src/components/UserClass.js
@@ -4,7 +4,9 @@ class UserClass extends React.Component {
   constructor(props) {
     super(props);
   }
+
   render() {
+    //const { name } = this.props;
     return (
       <div className="user-card">
         <h1>Name: {this.props.name}</h1>
