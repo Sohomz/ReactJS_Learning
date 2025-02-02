7cad16caf7a5170918b31ee74920c43407043dce
Sohomz <sohomd088@gmail.com>
2025-02-02 17:03:20 +0530

    1:02

--- 11_NR_Mock_Web/src/components/About.js
+++ 11_NR_Mock_Web/src/components/About.js
@@ -16,18 +16,20 @@ class About extends React.Component {
   constructor(props) {
     super(props);
     this.props = props;
-    console.log("Parent constructor");
   }
+
+  async componentDidMount() {
+    console.log("Parent mounted");
+  }
+
   render() {
-    console.log("paremt render");
     return (
       <div>
         <h1>This is About page</h1>
-        <User name={"Sohom Das (function)"} />
-        <UserClass name={"Sohom Das (class)"} />
+        <User name="SohomDas (function)" />
+        <UserClass />
       </div>
     );
   }
 }
-
 export default About;
--- 11_NR_Mock_Web/src/components/UserClass.js
+++ 11_NR_Mock_Web/src/components/UserClass.js
@@ -2,21 +2,28 @@ import React from "react";
 
 class UserClass extends React.Component {
   constructor(props) {
-    console.log("Child constructor");
     super(props);
     this.state = {
       count: 0,
       count1: 1,
+      data: "Dummy Name",
     };
   }
 
+  async componentDidMount() {
+    const gitApiData = await fetch("https://api.github.com/users/sohomz");
+    const convertedData = await gitApiData.json();
+    this.setState({
+      data: convertedData,
+    });
+  }
+
   render() {
-    console.log("Child render");
     return (
       <div className="user-card">
         <h1>Count: {this.state.count}</h1>
-        <h1>Name: {this.props.name}</h1>
-        <h3>Location: Hooghly</h3>
+        <h1>Name: {this.state.data.name + " from gitApi"}</h1>
+        <h3>Location: {this.state.data.html_url}</h3>
         <h3>Contact: sohomd077@gmail.com</h3>
         <input
           type="button"
